e8428fab2e87ab4207a226e0926afe63edb6c006
Michael Guirguis <guirguis@mguirguis.com>
2022-04-08 16:19:32 -0400

    Setup tests

---
+++ src/tests/Genspark/MainTest.java
@@ -0,0 +1,28 @@
+package Genspark;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+class MainTest {
+
+    @BeforeEach
+    void setUp() {
+
+    }
+
+    @Test
+    void gameState() {
+    }
+
+    @Test
+    void setState() {
+    }
+
+    @Test
+    void noMatch() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
