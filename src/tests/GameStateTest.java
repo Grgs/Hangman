ba320f6895f82017d7780e284f95bdb656cb6920
Michael Guirguis <guirguis@mguirguis.com>
2022-04-23 22:02:26 -0400

    formatting

--- src/tests/GameStateTest.java
+++ src/tests/GameStateTest.java
@@ -1,10 +1,11 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GameStateTest {
     GameState gameState;
+
     @BeforeEach
     void setUp() {
         gameState = new GameState();
